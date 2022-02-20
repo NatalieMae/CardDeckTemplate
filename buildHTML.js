f4f6f0d86f660326ba8081534f9ceea020be20d8
natalie <nataliemae263@gmail.com>
2022-02-19 23:10:41 -0600

    tried to get the pics to appear but I think i am brain dead, I will try again tomorrow

--- buildHTML.js
+++ buildHTML.js
@@ -1,28 +1,42 @@
+/** @format */
 
 function renderDeck(myDeck) {
-    document.getElementById("deck").innerHTML = "";
+	document.getElementById("deck").innerHTML = "";
+	
+	for (let i = 0; i < deck.length; i++) {
+		let card = document.createElement("div");
 
-	for(let i = 0; i < deck.length; i++)
-	{
-		card.className = "card";
-		value.className = "value";
-		suit.className = "suit" + deck[i].Suit;
-
+		// card.textContent = "🂡";
+		// card.className = "card";
+		// value.className = "value";
+		// suit.className = "suit" + deck[i].suit;
+			
+	if (deck.deck[i].suit === "Hearts") {
+		card.textContent  = "&hearts";
+	} else if (deck.deck[i].suit === "Clubs") {
+		card.textContent = "&Clubs";
+	} else if (deck.deck[i].suit === "Spades") {
+		card.textContent = "&Spades";
+	}
 		value.innerHTML = deck[i].Value;
 		card.appendChild(value);
-		card.appendChild(suit);
+		card.appendChild(suit);	
 
-		let card = document.createElement("div");
-		if (deck.deck[i].suit === "Spades") {
-		
-		if(deck.deck[i].value === "Ace") card.textContent = "Ace of 🂡";
-		if(deck.deck[i].value === 2) card.textContent = "2 of 🂡"
+	if (deck.deck[i].suit === "Spades") {
+	  if (deck.deck[i].value === "Ace") card.textContent = "Ace of 🂡";
+	  if (deck.deck[i].value === 2) card.textContent = "2 of 🂡";
 		}
+
+
+
+		else if (deck.deck[i].suits === 'Diamonds') {
+	  if (deck.deck[i].value === "Ace") card.textContent = "Ace of ";
+			}
+			
 		myDeck.appendChild(card);
 	}
 	root.append(myDeck);
 }
-
 
 renderDeck(deck1);
 
@@ -31,4 +45,5 @@ warDiv.id = "warDiv";
 root.append(warDiv);
 
 renderDeck(deck2);
+
 
